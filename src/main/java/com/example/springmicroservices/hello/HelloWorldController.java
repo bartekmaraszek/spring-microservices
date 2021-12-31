ec57e02ce5b2421bf360895b69aa9f9485b7bdb3
Bart Maraszek <b.maraszek@gmail.com>
2021-12-31 18:09:17 +0000

    Simple controller

--- src/main/java/com/example/springmicroservices/hello/HelloWorldController.java
+++ src/main/java/com/example/springmicroservices/hello/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.springmicroservices.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello from a Bean");
+    }
+
+    @GetMapping("/hello/{name}")
+    public HelloWorldBean helloName(@PathVariable String name) {
+        return new HelloWorldBean("Hello " + name + "!");
     }
 }
