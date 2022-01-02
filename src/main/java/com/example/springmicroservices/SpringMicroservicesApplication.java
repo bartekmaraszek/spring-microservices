cc79ffba3df4f7ed92f66c2ebaea0d6e7329a78c
Bart Maraszek <b.maraszek@gmail.com>
2022-01-02 00:13:37 +0000

    Redo .gitignore

--- src/main/java/com/example/springmicroservices/SpringMicroservicesApplication.java
+++ src/main/java/com/example/springmicroservices/SpringMicroservicesApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMicroservicesApplication {
 
     public static void main(String[] args) {
-        try {
             SpringApplication.run(SpringMicroservicesApplication.class, args);
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
     }
 
 }
